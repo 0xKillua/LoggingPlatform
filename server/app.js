6ff62764577709286e880967edaee52b48f678db
Peter-1112 <choimingwang@gmail.com>
2022-06-27 16:31:02 +0800

    update

--- server/app.js
+++ server/app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 const db = require("./mongodb.js");
+const cors = require("cors");
 
 db(); // start server;
 const apiRoutes = require("./routes/routes.api");
+app.use(cors());
 app.use(express.json());
-app.get("/", (res, req) => {
-  req.send("<h1>This is main Page</h>");
-});
 
 app.use("/api", apiRoutes);
-app.listen(3001);
+app.listen(3003);
